e250fe492f087a5f5ca0791f639f941aa776ab6c
youknowwgit <choiyh2001@naver.com>
2026-08-29 14:58:09 +0900

    📦 Chore: 프로젝트 초기 설정

---
+++ src/main/java/com/homerun/HomerunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.homerun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomerunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomerunBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homerun/HomerunBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homerun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomerunBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
